f97d2caaed4772eb35eeec8d8cc9ead848e28c42
Yone <yone.moreno101@alu.ulpgc.es>
2018-08-11 10:31:40 +0100

    Mover dos cubos, movel el suelo a la izquierda y hacerlo grande al tocar enemigo.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float scale = 2f;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var transformLocalScale = other.transform.localScale;
+        //print(other.transform.localScale);       
+        transformLocalScale.x *= scale;
+        transformLocalScale.y *= scale;
+        other.transform.localScale = transformLocalScale;
+        //print(other.transform.localScale);
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    /// <summary>
+    /// Define el número de jugador
+    /// </summary>
+    public enum NumPlayer
+    {
+        player1 = 0,
+        player2 = 1
+    }
+
+    /// <summary>
+    /// Número de jugador
+    /// </summary>
+    public NumPlayer numPlayer;
+
+    public float jumpSpeed = 50f;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (numPlayer == NumPlayer.player1)
+        {
+            rb.AddForce((transform.up *
+                         Time.deltaTime *
+                         jumpSpeed *
+                         Input.GetAxis("VerticalPlayer1")), ForceMode.Impulse);
+        }
+
+        if (numPlayer == NumPlayer.player2)
+        {
+            rb.AddForce((transform.up *
+                         Time.deltaTime *
+                         jumpSpeed *
+                         Input.GetAxis("VerticalPlayer2")), ForceMode.Impulse);
+        }
+    }
+}
